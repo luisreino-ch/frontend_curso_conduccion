05d66f7c72aa6a8483ffc3db8ab22d253d7cc8c9
Luis Reino <105605562+luisreino-ch@users.noreply.github.com>
2023-08-30 18:24:57 -0500

    Update login.component.spec.ts
    mejora-login-ts

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -7,15 +7,24 @@ describe('LoginComponent', () => {
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    setupTestBed();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  function setupTestBed(): void {
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+    });
+
+    initializeComponentAndFixture();
+  }
+
+  function initializeComponentAndFixture(): void {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }
 });
