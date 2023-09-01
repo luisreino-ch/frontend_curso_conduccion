b0ee89512c8ddcc442b54cc968ecb5460c8bb4fc
Selena26 <serivas@espe.edu.ec>
2023-08-31 23:39:13 -0500

    1

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private URL = 'http://localhost:3000';
+
+  constructor(
+    private http:HttpClient, 
+    private jwtHelper:JwtHelperService
+  ) { }
+
+  singin(user:any){
+    return this.http.post(`${this.URL}/user/singin`,user);
+  }
+
+  isAuth():boolean{
+    const token = localStorage.getItem('token');
+    if(this.jwtHelper.isTokenExpired(token) || !localStorage.getItem('token')){
+      return false;
+    }
+    return true;
+  }
+
+}
